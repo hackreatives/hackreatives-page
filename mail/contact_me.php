b8a280fe69152529b14af59b2a5c91eebf878f43
hackreatives <desarrollo.hackreatives@gmail.com>
2021-06-28 14:39:00 -0500

    correccion php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "marketing.hackreatives@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Contacto Hackreatives:  $name";
 $body = "Haz recibido un nuevo contacto desde tu sitio web.\n\n"."Detalles:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@hackreatives.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: marketing.hackreatives@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
